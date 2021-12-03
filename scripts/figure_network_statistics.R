7bdfe0295b5457bc56b92fb7d3728ebb4efa23c2
danielseussler <daniel.seussler@gmail.com>
2021-12-03 19:36:06 +0100

    added mining data

--- scripts/figure_network_statistics.R
+++ scripts/figure_network_statistics.R
@@ -15,10 +15,12 @@ load("data/out/country_list.RData")
 load("data/out/sipri_tiv.RData")
 
 baci_aggregated = readRDS("data/out/baci_aggregated.rds")
+itpd_mining = readRDS("data/out/itpd_mining-energy.rds")
 
 period = 1995:2018
-networks_yearly_arms = list()
-networks_yearly_trade = list()
+networks_yearly_sipri = list()
+networks_yearly_cepii = list()
+networks_yearly_itpd = list()
 
 # some data frames are in the time period 1950:2018 
 # trade data is only available for 1995:2018
@@ -32,16 +34,26 @@ for(i in 1:length(period)){
   set.edge.value(net, "TIV (in thousands)", sipri_tiv[[j]][ind, ind])
   set.vertex.attribute(net, "index", country_list$ID[ind])
   set.vertex.attribute(net, "iso3", country_list$iso3[ind])
-  networks_yearly_arms[[i]] = net
+  networks_yearly_sipri[[i]] = net
   
   net = network(baci_aggregated[[i]][ind, ind])
   set.edge.value(net, "Trade Flow", baci_aggregated[[i]][ind, ind])
   set.vertex.attribute(net, "index", country_list$ID[ind])
   set.vertex.attribute(net, "iso3", country_list$iso3[ind])
-  networks_yearly_trade[[i]] = net
+  networks_yearly_cepii[[i]] = net
   
 }
 
+for(i in 1:length(2000:2016)){
+  j = i + 1999 - 1949
+  ind = (EX[, j] == 1) 
+  
+  net = network(itpd_mining[[i]][ind, ind])
+  set.edge.value(net, "ITPD Mining/Energy", itpd_mining[[i]][ind, ind])
+  set.vertex.attribute(net, "index", country_list$ID[ind])
+  set.vertex.attribute(net, "iso3", country_list$iso3[ind])
+  networks_yearly_itpd[[i]] = net
+}
 
 # plot networks
 # needs improvements, take in / outdegree for nodal size
@@ -51,29 +63,38 @@ pdf("figures/network_plots.pdf", paper = "a4")
 par(mfrow = c(2,2))
 
 for (i in c(1, 6, 11, 16)) {
-  plot(networks_yearly_arms[[i]],
+  plot(networks_yearly_sipri[[i]],
        displayisolates = FALSE,  
        label.pos=5,  label.cex = 0.6, 
        vertex.cex = 2.2, pad = 0.5,
-       label = networks_yearly_arms[[i]] %v% "iso3", 
+       label = networks_yearly_sipri[[i]] %v% "iso3", 
        main = paste("International Arms Transfers in", i + 1994))
 }
 
 for (i in c(1, 6, 11, 16)) {
-  plot(networks_yearly_trade[[i]],
+  plot(networks_yearly_cepii[[i]],
        displayisolates = FALSE,  
        label.pos=5,  label.cex = 0.6, 
        vertex.cex = 2.2, pad = 0.5,
-       label = networks_yearly_trade[[i]] %v% "iso3", 
+       label = networks_yearly_cepii[[i]] %v% "iso3", 
        main = paste("International Trade in", i + 1994))
+}
+
+for (i in c(1, 6, 11)) {
+  plot(networks_yearly_itpd[[i]],
+       displayisolates = FALSE,  
+       label.pos=5,  label.cex = 0.6, 
+       vertex.cex = 2.2, pad = 0.5,
+       label = networks_yearly_itpd[[i]] %v% "iso3", 
+       main = paste("International Trade Energy / Miningin", i + 1994))
 }
 dev.off()
 
 
 # create networkDynamic object
 # By default, each observation is assumed to span a unit interval, (so the 1st goes from 0 to 1, 2nd from 1-2, etc).
-networks_dynamic_arms = networkDynamic(network.list = networks_yearly_arms, vertex.pid = 'index', start = 1995, end = 2018)
-# networks_dynamic_trade = networkDynamic(network.list = networks_yearly_trade, vertex.pid = 'index', start = 1995, end = 2018)
+networks_dynamic_arms = networkDynamic(network.list = networks_yearly_sipri, vertex.pid = 'index', start = 1995, end = 2018)
+# networks_dynamic_trade = networkDynamic(network.list = networks_yearly_cepii, vertex.pid = 'index', start = 1995, end = 2018)
 
 
 
@@ -88,77 +109,80 @@ par(mfrow = c(3, 2))
 #lines(tEdgeFormation(networks_dynamic_trade, start = 1995, end = 2018))
 
 plot(tErgmStats(networks_dynamic_arms, '~edges', start = 1995, end = 2018), main = "Arms Transfers: Edge Count")
-plot(1995:2018, unlist(lapply(networks_yearly_trade, FUN = function(x){network.edgecount(x)})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Edge Count")
+plot(1995:2018, unlist(lapply(networks_yearly_cepii, FUN = function(x){network.edgecount(x)})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Edge Count")
 
 plot(tSnaStats(networks_dynamic_arms, 'gden', start = 1995, end = 2018), main = "Arms Transfers: Network Density")
-plot(1995:2018, unlist(lapply(networks_yearly_trade, FUN = function(x){network.density(x)})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Network Density")
+plot(1995:2018, unlist(lapply(networks_yearly_cepii, FUN = function(x){network.density(x)})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Network Density")
 
 plot(tSnaStats(networks_dynamic_arms, 'grecip', measure = "dyadic.nonnull", start = 1995, end = 2018), main = "Arms Transfers: Network Reciprocity")
-plot(1995:2018, unlist(lapply(networks_yearly_trade, FUN = function(x){grecip(x, measure = "dyadic.nonnull")})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Network Reciprocity")
+plot(1995:2018, unlist(lapply(networks_yearly_cepii, FUN = function(x){grecip(x, measure = "dyadic.nonnull")})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Network Reciprocity")
 
 plot(tSnaStats(networks_dynamic_arms, 'gtrans', start = 1995, end = 2018), main = "Arms Transfers: Network Transitivity")
-plot(1995:2018, unlist(lapply(networks_yearly_trade, FUN = function(x){gtrans(x)})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Network Transitivity")
+plot(1995:2018, unlist(lapply(networks_yearly_cepii, FUN = function(x){gtrans(x)})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Network Transitivity")
 
 plot(tSnaStats(networks_dynamic_arms, "centralization", FUN = "betweenness", start = 1995, end = 2018), main = "Arms Transfers: Betweenness-centrality")
-plot(1995:2018, unlist(lapply(networks_yearly_trade, FUN = function(x){centralization(x, FUN = "betweenness")})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Betweenness-centrality")
+plot(1995:2018, unlist(lapply(networks_yearly_cepii, FUN = function(x){centralization(x, FUN = "betweenness")})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Betweenness-centrality")
 
 plot(tSnaStats(networks_dynamic_arms, "centralization", FUN = "degree", cmode = "indegree", start = 1995, end = 2018), main = "Arms Transfers: In-centrality")
-plot(1995:2018, unlist(lapply(networks_yearly_trade, FUN = function(x){centralization(x, FUN = "degree", cmode = "indegree")})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: In-centrality")
+plot(1995:2018, unlist(lapply(networks_yearly_cepii, FUN = function(x){centralization(x, FUN = "degree", cmode = "indegree")})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: In-centrality")
 
 plot(tSnaStats(networks_dynamic_arms, "centralization", FUN = "degree", cmode = "outdegree", start = 1995, end = 2018), main = "Arms Transfers: Out-centrality")
-plot(1995:2018, unlist(lapply(networks_yearly_trade, FUN = function(x){centralization(x, FUN = "degree", cmode = "outdegree")})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Out-centrality")
+plot(1995:2018, unlist(lapply(networks_yearly_cepii, FUN = function(x){centralization(x, FUN = "degree", cmode = "outdegree")})), type = "l", xlab = "Time", ylab = "Series 1", main = "Conventional Trade: Out-centrality")
 
 plot(tErgmStats(networks_dynamic_arms, '~idegree(d=1)', start = 1995, end = 2018), main = "Single Supplier")
-plot(1995:2018, unlist(lapply(networks_yearly_arms, FUN = function(x){network.size(x)})),  type = "l", xlab = "Time", ylab = "Series 1", main="Network Size")
-plot(1995:2018, unlist(lapply(networks_yearly_arms, FUN = function(x){sum(get.edge.value(x,"TIV (in thousands)"))})),  type = "l", xlab = "Time", ylab = "Series 1", main="Sum TIV (in thousands)")
-plot(1995:2018, unlist(lapply(networks_yearly_trade, FUN = function(x){sum(get.edge.value(x,"Trade Flow"))})),  type = "l", xlab = "Time", ylab = "Series 1", main="Sum of Conventional Trade Flow")
+plot(1995:2018, unlist(lapply(networks_yearly_sipri, FUN = function(x){network.size(x)})),  type = "l", xlab = "Time", ylab = "Series 1", main="Network Size")
+plot(1995:2018, unlist(lapply(networks_yearly_sipri, FUN = function(x){sum(get.edge.value(x,"TIV (in thousands)"))})),  type = "l", xlab = "Time", ylab = "Series 1", main="Sum TIV (in thousands)")
+plot(1995:2018, unlist(lapply(networks_yearly_cepii, FUN = function(x){sum(get.edge.value(x,"Trade Flow"))})),  type = "l", xlab = "Time", ylab = "Series 1", main="Sum of Conventional Trade Flow")
 
+plot(2000:2016, unlist(lapply(networks_yearly_itpd, FUN = function(x){network.edgecount(x)})), type = "l", xlab = "Time", ylab = "Series 1", main = "ITPD Mining/Energy: Edge Count")
+plot(2000:2016, unlist(lapply(networks_yearly_itpd, FUN = function(x){network.density(x)})), type = "l", xlab = "Time", ylab = "Series 1", main = "ITPD Mining/Energy: Network Density")
+plot(2000:2016, unlist(lapply(networks_yearly_itpd, FUN = function(x){sum(get.edge.value(x,"ITPD Mining/Energy"))})),  type = "l", xlab = "Time", ylab = "Series 1", main="ITPD Mining/Energy Trade Value Sum")
 
-
+plot.new()
 # In- and Outdegree Distributions
 # to fix: degree = 1 should be 0, log transform issues. Simply new labels?
 
-in_degrees1 = unlist(lapply(networks_yearly_arms[(1995:2005)-1994], FUN = function(x){degree(x,cmode="indegree")}))
-in_degrees2 = unlist(lapply(networks_yearly_trade[(1995:2005)-1994], FUN = function(x){degree(x,cmode="indegree")}))
+in_degrees1 = unlist(lapply(networks_yearly_sipri[(1995:2005)-1994], FUN = function(x){degree(x,cmode="indegree")}))
+in_degrees2 = unlist(lapply(networks_yearly_cepii[(1995:2005)-1994], FUN = function(x){degree(x,cmode="indegree")}))
 tmp1 = table(in_degrees1) / length(in_degrees1)
 tmp2 = table(in_degrees2) / length(in_degrees2)
 dat = list(as.matrix(tmp1), as.matrix(tmp2))
 
 plot(unlist(dat),type="n", log = "x", xlim=c(1,max(sapply(dat,length))), ylab = "Proportion", xlab = "Degree", main = "In-Degree Distribution 1995-2005 (log-scale)")
 mapply(lines, dat, col=seq_along(dat), type = "b", pch = 20)
-legend("topright", c("Arms", "Trade"), lty=1, col=seq_along(dat))
+legend("topright", c("SIPRI", "CEPII"), lty=1, col=seq_along(dat))
 
 
-out_degrees1 = unlist(lapply(networks_yearly_arms[(1995:2005)-1994], FUN = function(x){degree(x,cmode="outdegree")}))
-out_degrees2 = unlist(lapply(networks_yearly_trade[(1995:2005)-1994], FUN = function(x){degree(x,cmode="outdegree")}))
+out_degrees1 = unlist(lapply(networks_yearly_sipri[(1995:2005)-1994], FUN = function(x){degree(x,cmode="outdegree")}))
+out_degrees2 = unlist(lapply(networks_yearly_cepii[(1995:2005)-1994], FUN = function(x){degree(x,cmode="outdegree")}))
 tmp1 = table(out_degrees1) / length(out_degrees1)
 tmp2 = table(out_degrees2) / length(out_degrees2)
 dat = list(as.matrix(tmp1), as.matrix(tmp2))
 
 plot(unlist(dat), type="n", log = "x", xlim=c(1,max(sapply(dat,length))), ylab = "Proportion", xlab = "Degree", main = "Out-Degree Distribution 1995-2005 (log-scale)")
 mapply(lines,dat, col=seq_along(dat), type = "b", pch = 20)
-legend("topright", c("Arms", "Trade"), lty=1, col=seq_along(dat))
+legend("topright", c("SIPRI", "CEPII"), lty=1, col=seq_along(dat))
 
 
-in_degrees1 = unlist(lapply(networks_yearly_arms[(2005:2018)-1994], FUN = function(x){degree(x,cmode="indegree")}))
-in_degrees2 = unlist(lapply(networks_yearly_trade[(2005:2018)-1994], FUN = function(x){degree(x,cmode="indegree")}))
+in_degrees1 = unlist(lapply(networks_yearly_sipri[(2005:2018)-1994], FUN = function(x){degree(x,cmode="indegree")}))
+in_degrees2 = unlist(lapply(networks_yearly_cepii[(2005:2018)-1994], FUN = function(x){degree(x,cmode="indegree")}))
 tmp1 = table(in_degrees1) / length(in_degrees1)
 tmp2 = table(in_degrees2) / length(in_degrees2)
 dat = list(as.matrix(tmp1), as.matrix(tmp2))
 
 plot(unlist(dat), type="n", log = "x", xlim=c(1,max(sapply(dat,length))), ylab = "Proportion", xlab = "Degree", main = "In-Degree Distribution 2005-2018 (log-scale)")
 mapply(lines,dat, col=seq_along(dat), type = "b", pch = 20)
-legend("topright", c("Arms", "Trade"), lty=1, col=seq_along(dat))
+legend("topright", c("SIPRI", "CEPII"), lty=1, col=seq_along(dat))
 
 
-out_degrees1 = unlist(lapply(networks_yearly_arms[(2005:2018)-1994], FUN = function(x){degree(x,cmode="outdegree")}))
-out_degrees2 = unlist(lapply(networks_yearly_trade[(2005:2018)-1994], FUN = function(x){degree(x,cmode="outdegree")}))
+out_degrees1 = unlist(lapply(networks_yearly_sipri[(2005:2018)-1994], FUN = function(x){degree(x,cmode="outdegree")}))
+out_degrees2 = unlist(lapply(networks_yearly_cepii[(2005:2018)-1994], FUN = function(x){degree(x,cmode="outdegree")}))
 tmp1 = table(out_degrees1) / length(out_degrees1)
 tmp2 = table(out_degrees2) / length(out_degrees2)
 dat = list(as.matrix(tmp1), as.matrix(tmp2))
 
 plot(unlist(dat), type="n", log = "x", xlim=c(1,max(sapply(dat,length))), ylab = "Proportion", xlab = "Degree", main = "Out-Degree Distribution 2005-2018 (log-scale)")
 mapply(lines,dat, col=seq_along(dat), type = "b", pch = 20)
-legend("topright", c("Arms", "Trade"), lty=1, col=seq_along(dat))
+legend("topright", c("SIPRI", "CEPII"), lty=1, col=seq_along(dat))
 
 dev.off()
