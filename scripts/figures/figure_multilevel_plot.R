caf4ef305624b02315a879db1c8ddd68b45072f0
danielseussler <daniel.seussler@gmail.com>
2022-04-16 17:56:53 +0200

    added multilevel figure and summary statistics for sipri

--- scripts/figures/figure_multilevel_plot.R
+++ scripts/figures/figure_multilevel_plot.R
@@ -1,40 +1,60 @@
+#------------------------------------------------------------------------------#
 # A Network Plot for the Multilevel Network in 2003
+#------------------------------------------------------------------------------#
+
+# Corresponding Tutorials: 
+# http://mr.schochastics.net/netVizR.html (for igraph / ggraph)
+# http://blog.schochastics.net/post/visualizing-multilevel-networks-with-graphlayouts/
 
 library(igraph)
 library(ggraph)
 library(graphlayouts)
 
-
-set.seed(1234)
 rm(list = ls(all.names = TRUE))
 
 
+# setup
+source("utils/utils.R")
+source("utils/construct_header.R")
+source("utils/custom_trade_to_binary.R")
+path <- data_path.get()
+set.seed(1234)
+
+
 # load data 
-load("data/out/EX.RData")
-load("data/out/colony.RData")
-load("data/out/country_list.RData")
-load("data/out/sipri_tiv.RData")
-baci_aggregated = readRDS("data/out/baci_aggregated.rds")
+load(file.path(path, "out/EX.RData"))
+load(file.path(path, "out/colony.RData"))
+load(file.path(path, "out/country_list.RData"))
+sipri_tiv <- readRDS(file.path(path, "out/sipri_tiv.rds"))
+trade <- readRDS(file.path(path, "out/baci_aggregated.rds"))
 
-thrshld1 = 0
-thrshld2 = 100 
-year = 2001
-present = (EX[, year - 1949] == 1)
-n = sum(present)
+thrshld1 <- 0
+thrshld2 <- 100
+year <- 2003
+start <- 1995
+end <- 2018
 
-mat1 = (sipri_tiv[[year - 1949]][present, present] > thrshld1) * 1
-mat2 = (baci_aggregated[[year - 1994]][present, present] > thrshld2) * 1
-diag = diag(1, n, n)
-colnames(diag) = country_list$V1[present]
-rownames(diag) = country_list$V1[present]
-mat = rbind(cbind(mat2, diag), cbind(diag, mat1))
-  
-net = graph_from_adjacency_matrix(mat, mode = "directed", diag = FALSE)
-net = set_vertex_attr(net, "lvl", index = V(net), c(rep(1, n), rep(2, n)))
+# selection of countries included: present over the complete time horizon (cf. soam)
+included <- rowSums(EX[, (start:end) - 1949]) == length(start:end)
+n <- sum(included)
+
+mat1 <- (sipri_tiv[[year - 1949]][included, included] > thrshld1) * 1
+mat2 <- (trade[[year - 1994]][included, included] > thrshld1) * 1
+
+diag <- diag(1, n, n)
+
+colnames(diag) <- country_list$V1[included]
+rownames(diag) <- country_list$V1[included]
+
+mat <- rbind(cbind(mat2, diag), cbind(diag, mat1))
+
+net <- graph_from_adjacency_matrix(mat, mode = "directed", diag = FALSE)
+net <- set_vertex_attr(net, "lvl", index = V(net), c(rep(1, n), rep(2, n)))
 vertex_attr_names(net)
 
+
 dev.new()
-pdf(file = "figures/plot_multilevel_2001.pdf", paper = "a4r")
+pdf(file = "figures/figure_descriptives_multilevel_2003.pdf", paper = "a4r", width = sqrt(2) * 10, height = 10 )
 xy <- layout_as_multilevel(net, type = "fix1", FUN1 = layout_with_stress, alpha = 25, beta = 45)
 ggraph(net, "manual", x = xy[, 1], y = xy[, 2]) +
   geom_edge_link0(
@@ -66,41 +86,4 @@ dev.off()
 
 
 
-# http://mr.schochastics.net/netVizR.html (for igraph / ggraph)
-# http://blog.schochastics.net/post/visualizing-multilevel-networks-with-graphlayouts/
-data("multilvl_ex", package = "graphlayouts")
-vertex_attr_names(multilvl_ex)
-vertex_attr(multilvl_ex, "lvl", index = V(multilvl_ex))
 
-pdf(file = "figures/test.pdf", paper = "a4r")
-cols2 <- c("#3A5FCD", "#CD00CD", "#EE30A7", "#EE6363", 
-           "#CD2626", "#458B00", "#EEB422", "#EE7600")
-
-xy <- layout_as_multilevel(multilvl_ex,type = "all", alpha = 25, beta = 45)
-ggraph(multilvl_ex, "manual", x = xy[, 1], y = xy[, 2]) +
-  geom_edge_link0(
-    aes(filter = (node1.lvl == 1 & node2.lvl == 1)),
-    edge_colour = "firebrick3",
-    alpha = 0.5,
-    edge_width = 0.3
-  ) +
-  geom_edge_link0(
-    aes(filter = (node1.lvl != node2.lvl)),
-    alpha = 0.3,
-    edge_width = 0.1,
-    edge_colour = "black"
-  ) +
-  geom_edge_link0(
-    aes(filter = (node1.lvl == 2 &
-                    node2.lvl == 2)),
-    edge_colour = "goldenrod3",
-    edge_width = 0.3,
-    alpha = 0.5
-  ) +
-  geom_node_point(aes(shape = as.factor(lvl)), fill = "grey25", size = 3) +
-  scale_shape_manual(values = c(21, 22)) +
-  theme_graph() +
-  coord_cartesian(clip = "off", expand = TRUE) +
-  theme(legend.position = "none")
-
-dev.off()
---
+++ scripts/figures/table_summary_sipri.R
@@ -0,0 +1,113 @@
+#------------------------------------------------------------------------------#
+# Summary table to see whats in SIPRI data
+#------------------------------------------------------------------------------#
+
+# This script reruns the SIPRI Data Aggregation and filtes for the included countries.
+
+library(readr)
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(kableExtra)
+
+
+rm(list = ls(all.names = TRUE))
+
+
+# setup
+source("utils/utils.R")
+source("utils/construct_header.R")
+source("utils/custom_trade_to_binary.R")
+path <- data_path.get()
+set.seed(1234)
+
+
+# load data 
+load(file.path(path, "out/EX.RData"))
+load(file.path(path, "out/colony.RData"))
+load(file.path(path, "out/country_list.RData"))
+
+# setup 
+year <- 2003
+start <- 1995
+end <- 2018
+
+# selection of countries included: present over the complete time horizon (cf. soam)
+included <- rowSums(EX[, (start:end) - 1949]) == length(start:end)
+n <- sum(included)
+
+
+# load sipri data
+paths <- list(
+  file.path(path, "raw/SIPRI/1950_1970.txt"),
+  file.path(path, "raw/SIPRI/1971_1980.txt"),
+  file.path(path, "raw/SIPRI/1981_1990.txt"),
+  file.path(path, "raw/SIPRI/1991_2000.txt"),
+  file.path(path, "raw/SIPRI/2001_2019.txt")
+)
+
+dfl = lapply(paths, read.csv, sep = ";", skip = 4) # Read the files into a list
+dfl = rbindlist(dfl) # Make one data.table out of the read files
+changeCols<- names(dfl)
+
+
+# Make factors to characters
+dfl[, (changeCols) := lapply(.SD, as.character), .SDcols = changeCols]
+
+
+# Unkown countries are deleted 
+dfl = dfl[dfl$Seller != ""]
+
+
+# There seems to be Problem with deals whose description includes in the raw data a ; in the Description 
+missing_tmp = which(suppressWarnings(is.na(as.numeric(as.character(dfl$Delivery.year)))))
+dfl[missing_tmp,6:16] = dfl[missing_tmp,7:17]
+
+dfl$Delivery.year = suppressWarnings(as.numeric(as.character(dfl$Delivery.year)))
+dfl$Order.date = suppressWarnings(as.numeric(as.character(dfl$Order.date)))
+
+dfl$Seller[dfl$Seller == "Cote d'Ivoire"] = "Cote dIvoire"
+dfl$Seller[dfl$Seller == "Czechia"] = "Czech Republic"
+dfl$Seller[dfl$Seller == "Macedonia"] = "Macedonia (FYROM)"
+dfl$Seller[dfl$Seller == "East Germany (GDR)"] = "German Democratic Republic"
+dfl$Seller[dfl$Seller == "Bosnia-Herzegovina"] = "Bosnia and Herzegovina"
+
+dfl$Buyer[dfl$Buyer == "Cote d'Ivoire"] = "Cote dIvoire"
+dfl$Buyer[dfl$Buyer == "Czechia"] = "Czech Republic"
+dfl$Buyer[dfl$Buyer == "Macedonia"] = "Macedonia (FYROM)"
+dfl$Buyer[dfl$Buyer == "East Germany (GDR)"] = "German Democratic Republic"
+dfl$Buyer[dfl$Buyer == "Bosnia-Herzegovina"] = "Bosnia and Herzegovina"
+
+
+# Change all transactions with the Soviet Union to Russia 
+dfl$Seller[dfl$Seller == "Soviet Union"] = "Russia" 
+dfl$Buyer[dfl$Buyer == "Soviet Union"] = "Russia" 
+
+dfl$SIPRI.estimate = as.numeric(dfl$SIPRI.estimate)
+dfl$TIV.deal.unit = as.numeric(dfl$TIV.deal.unit)
+dfl$TIV.delivery.values = as.numeric(dfl$TIV.delivery.values)
+
+
+# select transfers in our time frame 
+dfl = dfl[Order.date >= start & Order.date <= end]
+
+
+# select countries included in our analysis 
+dfl = dfl[Seller %in% country_list$V1 & Buyer %in% country_list$V1]
+
+dfl_summary = dfl[, .(Count = .N, "TIV (Deal Unit)" = sum(TIV.deal.unit, na.rm = TRUE)), by=c("Armament.category")]
+dfl_summary = dfl_summary[order(-Count)]
+setnames(dfl_summary, "Armament.category", "Armament Category")
+
+
+## Output
+
+sink("figures/table_overview_sipri.txt")
+
+kbl(dfl_summary, booktabs = T,  format = "latex", 
+    digits = 0,  escape = F, linesep = "",
+    caption = "Major Conventional Weapons Transfers (1995-2018)", label = "overview_sipri") %>%
+  kable_styling(full_width = FALSE) %>%
+  kable_styling(latex_options = "HOLD_position")
+
+sink()
